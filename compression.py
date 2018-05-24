3cf2c3e7ece5e28c3b6d5753fb24b0e2fb923880
Adrianna Pińska <adrianna.pinska@gmail.com>
2018-05-24 17:57:19 +0200

    made everything OO; absolute error -> fractional error

--- compression.py
+++ compression.py
@@ -12,13 +12,6 @@ import re
 import itertools
 import subprocess
 
-# This will go in the notebook
-#ZFP_EXEC = "zfp"
-#SZ_EXEC = "sz"
-#TMP_DIR = "/tmp"
-#DATA_DIR = "~/data"
-
-
 class Region:
     def __init__(self, data):
         self.data = data
@@ -70,9 +63,9 @@ class Region:
         self.data.astype(dtype).tofile(filename)
         
     def delta_errors(self, other):
-        delta = np.abs(self.data - other.data)
+        delta = np.abs(self.data - other.data) / np.abs(self.data)
         delta = (~np.isnan(self.data)) * delta
-        return np.nansum(delta), np.nanmax(delta)
+        return np.nanmean(delta), np.nanmax(delta)
     
     def colourmapped(self, colourmap, vmin=None, vmax=None):
         return ColourmappedRegion.from_data(self.data, colourmap, vmin, vmax)
@@ -114,123 +107,188 @@ class ColourmappedRegion:
         return ColourmappedRegion.from_data(region.data, self.colourmap, self.vmin, self.vmax)
         
     def delta_errors(self, other):
-        delta = np.abs(self.image - other.image)
+        delta = np.abs(self.image - other.image) / np.abs(self.image)
         delta = (~np.isnan(np.abs(self.image))) * delta
-        return np.nansum(delta), np.nanmax(delta)
+        return np.nanmean(delta), np.nanmax(delta)
 
-# TODO use temporary directory; make a class for this?
-# instantiate it; add properties for data directory, colourmap, etc?
-
-def ZFP_compress(region, image, *args):
-    width, height = region.data.shape
-    
-    region.write_to_file("original.arr", "f4")
-    
-    zip_p = subprocess.Popen(("zfp", "-i", "original.arr", "-2", str(width), str(height), "-f", *args, "-z", "-"), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    unzip_p = subprocess.Popen(("zfp", "-z", "-", "-2", str(width), str(height), "-f", *args ,"-o", "round_trip.arr"), stdin=zip_p.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    zip_p.stdout.close()
-    
-    m = re.search("zfp=(\d+)", unzip_p.communicate()[1].decode())
-    compressed_size = int(m.group(1))
-    
-    round_trip_region = Region.from_file("round_trip.arr", "f4", width, height)
-    
-    subprocess.run(("rm", "original.arr"))
-    subprocess.run(("rm", "round_trip.arr"))
-    
-    compressed_image = image.clone_colourmap_to(round_trip_region)
-    
-    return round_trip_region, compressed_image, compressed_size, None
-
-def ZFP_compress_fixed_rate(region, image, rate):
-    return ZFP_compress(region, image, "-r", str(rate))
-
-def ZFP_compress_fixed_precision(region, image, precision):
-    return ZFP_compress(region, image, "-p", str(precision))
-
-def ZFP_compress_fixed_accuracy(region, image, tolerance):
-    return ZFP_compress(region, image, "-a", str(tolerance))
+# TODO use temporary directory and executable paths
 
 
-def SZ_compress(region, image, *args):
-    width, height = region.data.shape
+class Compressor:
+    
+    def __init__(self, region, image, temp_dir=".", zfp="zfp", sz="sz"):
+        self.region = region
+        self.image = image
         
-    region.write_to_file("original.arr", "f4")
-    
-    subprocess.run(("sz", "-c", "sz.config", *args, "-f", "-z", "-i", "original.arr", "-2", str(width), str(height)), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    subprocess.run(("sz", "-c", "sz.config", *args, "-f", "-x", "-s", "original.arr.sz", "-2", str(width), str(height)), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    
-    compressed_size = os.stat("original.arr.sz").st_size
-    round_trip_region = Region.from_file("original.arr.sz.out", "f4", width, height)
+        self.temp_dir = os.path.abspath(temp_dir)
+        self.zfp = zfp
+        self.sz = sz
 
-    subprocess.run(("rm", "original.arr"))
-    subprocess.run(("rm", "original.arr.sz"))
-    subprocess.run(("rm", "original.arr.sz.out"))
-    
-    compressed_image = image.clone_colourmap_to(round_trip_region)
-    
-    return round_trip_region, compressed_image, compressed_size, None
-    
-def SZ_compress_PSNR(region, image, PSNR):
-    return SZ_compress(region, image, "-M", "PSNR", "-S", str(PSNR))
-
-def JPG_compress(region, image, *args):
-    image.to_jpg("compressed.jpg", args[0])
-    
-    compressed_size = os.stat("compressed.jpg").st_size
-    compressed_image = ColourmappedRegion.from_jpg("compressed.jpg")
-
-    subprocess.run(("rm", "compressed.jpg"))
-    
-    return None, compressed_image, None, compressed_size
-
-def JPG_compress_quality(region, image, quality):
-    return JPG_compress(region, image, quality)
-
-
-# TODO: regenerate the regions when we want to see one; maybe cache some later
-# TODO: how do we dynamically load the function? Keep a dictionary mapping of name to function? Put them all in a class so we can just getattr?
-
-ALGORITHMS = (
-    ("ZFP (Fixed rate)", ZFP_compress_fixed_rate, range(1, 32+1)),
-    ("ZFP (Fixed precision)", ZFP_compress_fixed_precision, range(1, 32+1)),
-    ("ZFP (Fixed accuracy)", ZFP_compress_fixed_accuracy, list(range(1, 21)) + [
-                0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
-                5e-2, 2e-2, 1e-2, 5e-3, 2e-3, 1e-3, 5e-4, 2e-4, 1e-4, 
-                5e-5, 2e-5, 1e-5, 5e-6, 2e-6, 1e-6, 5e-7, 2e-7, 1e-7
-            ]
-        ),
-    ("SZ (PSNR bounded)", SZ_compress_PSNR, range(60, 100)),
-    ("JPEG", JPG_compress_quality, range(60, 101)),
-)
-
-def compare_algorithms(region, colourmap):
-    results = {}
-    
-    original_raw_size = np.dtype("f4").itemsize * region.data.size
-    
-    image = region.colourmapped(colourmap)
-    image.to_png("original.png")
-    original_image_size = os.stat("original.png").st_size
-    
-    for label, function, params in ALGORITHMS:
-        results[label] = {}
+    def ZFP_compress(self, *args):
+        prev = os.getcwd()
+        os.chdir(self.temp_dir)
         
-        for p in params:
-            round_trip_region, compressed_image, compressed_raw_size, compressed_image_size = function(region, image, p)
-            
-            if round_trip_region:
-                raw_e_sum, raw_e_max = region.delta_errors(round_trip_region)
-            
-            image_e_sum, image_r_max = image.delta_errors(compressed_image)
-            
-            if compressed_raw_size:
-                size_fraction = compressed_raw_size/original_raw_size
-            elif compressed_image_size:
-                size_fraction = compressed_image_size/original_image_size
-                
-            results[label][p] = (raw_e_sum, raw_e_max, image_e_sum, image_r_max, size_fraction)
-                
-    subprocess.run(("rm", "original.png"))
+        width, height = self.region.data.shape
+        
+        self.region.write_to_file("original.arr", "f4")
+        
+        zip_p = subprocess.Popen((self.zfp, "-i", "original.arr", "-2", str(width), str(height), "-f", *args, "-z", "-"), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        unzip_p = subprocess.Popen((self.zfp, "-z", "-", "-2", str(width), str(height), "-f", *args ,"-o", "round_trip.arr"), stdin=zip_p.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        zip_p.stdout.close()
+        
+        m = re.search("zfp=(\d+)", unzip_p.communicate()[1].decode())
+        compressed_size = int(m.group(1))
+        
+        round_trip_region = Region.from_file("round_trip.arr", "f4", width, height)
+        
+        subprocess.run(("rm", "original.arr"))
+        subprocess.run(("rm", "round_trip.arr"))
+        
+        compressed_image = self.image.clone_colourmap_to(round_trip_region)
+        
+        os.chdir(prev)
+        return round_trip_region, compressed_image, compressed_size, None
 
-    return results
+    def ZFP_compress_fixed_rate(self, rate):
+        return self.ZFP_compress("-r", str(rate))
+
+    def ZFP_compress_fixed_precision(self, precision):
+        return self.ZFP_compress("-p", str(precision))
+
+    def ZFP_compress_fixed_accuracy(self, tolerance):
+        return self.ZFP_compress("-a", str(tolerance))
+
+    def SZ_compress(self, *args):
+        prev = os.getcwd()
+        os.chdir(self.temp_dir)
+        
+        width, height = self.region.data.shape
+            
+        self.region.write_to_file("original.arr", "f4")
+        
+        subprocess.run((self.sz, "-c", "sz.config", *args, "-f", "-z", "-i", "original.arr", "-2", str(width), str(height)), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        subprocess.run((self.sz, "-c", "sz.config", *args, "-f", "-x", "-s", "original.arr.sz", "-2", str(width), str(height)), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        
+        compressed_size = os.stat("original.arr.sz").st_size
+        round_trip_region = Region.from_file("original.arr.sz.out", "f4", width, height)
+
+        subprocess.run(("rm", "original.arr"))
+        subprocess.run(("rm", "original.arr.sz"))
+        subprocess.run(("rm", "original.arr.sz.out"))
+        
+        compressed_image = self.image.clone_colourmap_to(round_trip_region)
+        
+        os.chdir(prev)
+        return round_trip_region, compressed_image, compressed_size, None
+        
+    def SZ_compress_PSNR(self, PSNR):
+        return self.SZ_compress("-M", "PSNR", "-S", str(PSNR))
+
+    def JPG_compress(self, *args):
+        prev = os.getcwd()
+        os.chdir(self.temp_dir)
+        
+        self.image.to_jpg("compressed.jpg", args[0])
+        
+        compressed_size = os.stat("compressed.jpg").st_size
+        compressed_image = ColourmappedRegion.from_jpg("compressed.jpg")
+
+        subprocess.run(("rm", "compressed.jpg"))
+        
+        os.chdir(prev)
+        return None, compressed_image, None, compressed_size
+
+    def JPG_compress_quality(self, quality):
+        return self.JPG_compress(quality)
+
+
+class Comparator:
+    def __init__(self, results, compressor):
+        self.results = results
+        self.compressor = compressor
+
+    ALGORITHMS = (
+        ("ZFP (Fixed rate)", "ZFP_compress_fixed_rate", range(1, 32+1)),
+        ("ZFP (Fixed precision)", "ZFP_compress_fixed_precision", range(1, 32+1)),
+        ("ZFP (Fixed accuracy)", "ZFP_compress_fixed_accuracy", list(range(1, 21)) + [
+                    0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1,
+                    5e-2, 2e-2, 1e-2, 5e-3, 2e-3, 1e-3, 5e-4, 2e-4, 1e-4, 
+                    5e-5, 2e-5, 1e-5, 5e-6, 2e-6, 1e-6, 5e-7, 2e-7, 1e-7
+                ]
+            ),
+        ("SZ (PSNR bounded)", "SZ_compress_PSNR", range(60, 100)),
+        ("JPEG", "JPG_compress_quality", range(60, 101)),
+    )
+
+    @classmethod
+    def compare_algorithms(cls, region, colourmap, temp_dir=".", zfp="zfp", sz="sz"):
+        results = []
+        
+        original_raw_size = np.dtype("f4").itemsize * region.data.size
+        
+        image = region.colourmapped(colourmap)
+        image.to_png("original.png")
+        original_image_size = os.stat("original.png").st_size
+        
+        compressor = Compressor(region, image, temp_dir, zfp, sz)
+                
+        for label, function_name, params in cls.ALGORITHMS:
+            
+            for p in params:
+                round_trip_region, compressed_image, compressed_raw_size, compressed_image_size = getattr(compressor, function_name)(p)
+                
+                if round_trip_region:
+                    raw_error_mean, raw_error_max = region.delta_errors(round_trip_region)
+                else:
+                    raw_error_mean, raw_error_max = None, None
+                
+                image_error_mean, image_error_max = image.delta_errors(compressed_image)
+                
+                if compressed_raw_size:
+                    size_fraction = compressed_raw_size/original_raw_size
+                elif compressed_image_size:
+                    size_fraction = compressed_image_size/original_image_size
+                    
+                results.append({
+                    "label": label,
+                    "function_name": function_name, # may need it later to regenerate images
+                    "param": p, # may need it later to regenerate images
+                    "raw_error_mean": raw_error_mean, 
+                    "raw_error_max": raw_error_max, 
+                    "image_error_mean": image_error_mean, 
+                    "image_error_max": image_error_max, 
+                    "size_fraction": size_fraction,
+                })
+
+                    
+        subprocess.run(("rm", "original.png"))
+        
+        return cls(results, compressor)
+
+    # insufficiently generic; just inline these in the plot function
+    def get(self, fields, where):
+        return [[r[f] for f in fields] for r in self.results if all(r[k] == v for k, v in where.items())]
+    
+    def unique(self, field):
+        return {r[field] for r in self.results}
+        
+    def plot(self, xfield, yfield, xlabel, ylabel):
+
+        plt.close()
+        
+        for label in self.unique("label"):
+            xy = sorted(self.get((xfield, yfield), {"label": label})) # sort by xfield
+            x, y = zip(*xy)
+            
+            if all(v is None for v in y):
+                continue # skip e.g. non-existent raw errors for JPEG
+            
+            plt.plot(x, y, marker='o', ls='', label=label)
+            
+        plt.legend()
+        plt.xlabel(xlabel)
+        plt.ylabel(ylabel)
+        plt.show()
+        
+    def show_images(self, size_min, size_max):
+        # TODO: get all images within a size range and display them together with original image. Stacked? Raise on mouseover?
